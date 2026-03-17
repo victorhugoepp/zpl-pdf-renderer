751065bc8efff23b8ea56480a7954a1953b85ab5
victorhugoepp <164556528+victorhugoepp@users.noreply.github.com>
2026-03-17 12:22:56 -0300

    models.py

--- srcmodels.py
+++ srcmodels.py
@@ -1,1 +1,17 @@
+from parser import split_labels
 
+def main():
+
+    with open("sample.zpl", "r", encoding="utf-8") as f:
+        zpl = f.read()
+
+    labels = split_labels(zpl)
+
+    print("Total de etiquetas:", len(labels))
+
+    for i, label in enumerate(labels):
+        print(f"Etiqueta {i+1} tamanho:", len(label))
+
+
+if __name__ == "__main__":
+    main()
