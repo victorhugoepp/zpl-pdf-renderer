ccd687d7aa5cd391613b51e4b877ebe28380c369
victorhugoepp <164556528+victorhugoepp@users.noreply.github.com>
2026-03-17 12:22:35 -0300

    parser.py

--- srcparser.py
+++ srcparser.py
@@ -1,1 +1,8 @@
+import re
 
+def split_labels(zpl_text: str):
+    """
+    Divide o ZPL em múltiplas etiquetas.
+    Cada etiqueta começa com ^XA e termina com ^XZ
+    """
+    return re.findall(r'\^XA.*?\^XZ', zpl_text, flags=re.S)
